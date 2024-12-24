fe5fe732813c1dac6b5003ac72bae9b83fbe4dc7
Metronon <bumpercar07@gmail.com>
2024-12-24 12:22:30 +0900

    add admin to postComment

--- src/main/java/com/ll/auth/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/auth/domain/post/post/entity/Post.java
@@ -63,6 +63,7 @@ public class Post extends BaseTime {
         if (actor.equals(author)) return;
         throw new ServiceException("403-2", "작성자만 글을 삭제할 수 있습니다.");
     }
+
     public void checkActorCanModify(Member actor) {
         if (actor == null) throw new ServiceException("403-1", "로그인 후 이용해주세요.");
         if (actor.equals(author)) return;
